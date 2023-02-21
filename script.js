926da268151719f5bfd65fccdf4f9291fc1ced7c
KABIRAJ K <83573729+kabirajk@users.noreply.github.com>
2023-02-21 23:04:05 +0530

    missed page interation

--- script.js
+++ script.js
@@ -14,13 +14,14 @@ class JioSaavanApi{
         afterCall.getData=this.getData;
         this.getData(encodeURI(url),callback,function(resp){console.log(resp)},afterCall);
     }
-    searchSongs(callback,afterCall,page=1,limit=40){
+    searchSongs(callback,afterCall,page=1,limit=20){
         let self   =this;
         let query=this.searchbar.val();
         // query=query.toLowerCase();
         // query=query.replace(/\s/g,"+");
         if(self.lasturl.query == query){
-            self.lasturl.page+=1
+            self.lasturl.page+=1;
+            l=self.lasturl.page;
         }else{
             self.lasturl.query=query;
             self.lasturl.page=page
